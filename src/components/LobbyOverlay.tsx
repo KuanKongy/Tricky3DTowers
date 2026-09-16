cf7bf3f35dbc6e6b1aee6c01b031a9ff435402c4
KuanKongy <khanhpronam@gmail.com>
2026-09-16 14:28:00 -0700

    Game UI Fix 2

--- src/components/LobbyOverlay.tsx
+++ src/components/LobbyOverlay.tsx
@@ -206,9 +206,9 @@ export default function LobbyOverlay() {
             <div className="mt-1 leading-relaxed">
               {touchUI ? (
                 <>
-                  ❮ ❯ move 
+                  ◀	▶ move 
                   <br />⟳ rotate
-                  <br />⌄ soft drop
+                  <br />▼ soft drop
                   <br />
                   drag · aim camera
                   <br />
